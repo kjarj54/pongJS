92536ec8270343d88d7f874e74503e34d41a4552
Arauz <86898345+kjarj54@users.noreply.github.com>
2024-07-25 15:05:43 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,6 +4,8 @@ let altoPantalla = 400;
 let pelota;
 let jugador;
 let computadora;
+let puntajeJugador = 0;
+let puntajeComputadora = 0;
 
 function setup() {
   createCanvas(anchoPantalla, altoPantalla);
@@ -15,6 +17,12 @@ function setup() {
 function draw() {
   background(0);
 
+  // Dibujar bordes superiores e inferiores
+  fill(255);
+  rect(0, 0, width, 10); // borde superior
+  rect(0, height - 10, width, 10); // borde inferior
+
+  // Mostrar y mover pelota y raquetas
   pelota.mostrar();
   pelota.mover();
   pelota.rebotar(jugador);
@@ -25,6 +33,12 @@ function draw() {
 
   computadora.mostrar();
   computadora.moverAutomatica(pelota);
+
+  // Mostrar marcador
+  textSize(32);
+  fill(255);
+  text(puntajeJugador, width / 4, 40);
+  text(puntajeComputadora, 3 * width / 4, 40);
 }
 
 // Clase Pelota
@@ -35,10 +49,11 @@ class Pelota {
     this.diametro = 20;
     this.velocidadX = 5;
     this.velocidadY = 3;
+    this.color = [255, 255, 255];
   }
 
   mostrar() {
-    fill(255);
+    fill(this.color);
     ellipse(this.x, this.y, this.diametro);
   }
 
@@ -46,12 +61,17 @@ class Pelota {
     this.x += this.velocidadX;
     this.y += this.velocidadY;
 
-    if (this.y < 0 || this.y > height) {
+    if (this.y < 10 || this.y > height - 10) {
       this.velocidadY *= -1;
+      this.cambiarColor();
     }
 
     // Verificar si la pelota sale de la zona de juego
-    if (this.x < 0 || this.x > width) {
+    if (this.x < 0) {
+      puntajeComputadora++;
+      this.reiniciar();
+    } else if (this.x > width) {
+      puntajeJugador++;
       this.reiniciar();
     }
   }
@@ -70,6 +90,11 @@ class Pelota {
     this.y = height / 2;
     this.velocidadX = 5;
     this.velocidadY = 3;
+    this.color = [255, 255, 255];
+  }
+
+  cambiarColor() {
+    this.color = [random(255), random(255), random(255)];
   }
 }
 
@@ -92,7 +117,7 @@ class Raqueta {
   mover() {
     if (this.esJugador) {
       this.y = mouseY - this.alto / 2;
-      this.y = constrain(this.y, 0, height - this.alto);
+      this.y = constrain(this.y, 10, height - this.alto - 10); // Limitar posición de la raqueta
     }
   }
 
@@ -103,7 +128,7 @@ class Raqueta {
       } else if (pelota.y < this.y + this.alto / 2) {
         this.y -= this.velocidad;
       }
-      this.y = constrain(this.y, 0, height - this.alto);
+      this.y = constrain(this.y, 10, height - this.alto - 10); // Limitar posición de la raqueta
     }
   }
 }
