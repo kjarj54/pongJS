68531be7e74a73614bbad523f6f85ec08ee82e84
Arauz <86898345+kjarj54@users.noreply.github.com>
2024-07-25 14:49:40 -0600

    [INIT]

---
+++ sketch.js
@@ -0,0 +1,96 @@
+let anchoPantalla = 800;
+let altoPantalla = 400;
+
+let pelota;
+let jugador;
+let computadora;
+
+function setup() {
+  createCanvas(anchoPantalla, altoPantalla);
+  pelota = new Pelota();
+  jugador = new Raqueta(true); // Raqueta del jugador en la izquierda
+  computadora = new Raqueta(false); // Raqueta de la computadora en la derecha
+}
+
+function draw() {
+  background(0);
+
+  pelota.mostrar();
+  pelota.mover();
+  pelota.rebotar(jugador);
+  pelota.rebotar(computadora);
+  
+  jugador.mostrar();
+  jugador.mover();
+
+  computadora.mostrar();
+  computadora.moverAutomatica(pelota);
+}
+
+// Clase Pelota
+class Pelota {
+  constructor() {
+    this.x = width / 2;
+    this.y = height / 2;
+    this.diametro = 20;
+    this.velocidadX = 5;
+    this.velocidadY = 3;
+  }
+
+  mostrar() {
+    fill(255);
+    ellipse(this.x, this.y, this.diametro);
+  }
+
+  mover() {
+    this.x += this.velocidadX;
+    this.y += this.velocidadY;
+
+    if (this.y < 0 || this.y > height) {
+      this.velocidadY *= -1;
+    }
+  }
+
+  rebotar(raqueta) {
+    if (this.x - this.diametro / 2 < raqueta.x + raqueta.ancho && 
+        this.x + this.diametro / 2 > raqueta.x && 
+        this.y > raqueta.y && 
+        this.y < raqueta.y + raqueta.alto) {
+      this.velocidadX *= -1;
+    }
+  }
+}
+
+// Clase Raqueta
+class Raqueta {
+  constructor(esJugador) {
+    this.ancho = 10;
+    this.alto = 100;
+    this.x = esJugador ? 0 : width - this.ancho;
+    this.y = height / 2 - this.alto / 2;
+    this.velocidad = 5;
+    this.esJugador = esJugador;
+  }
+
+  mostrar() {
+    fill(255);
+    rect(this.x, this.y, this.ancho, this.alto);
+  }
+
+  mover() {
+    if (this.esJugador) {
+      if (keyIsDown(UP_ARROW)) {
+        this.y -= this.velocidad;
+      }
+      if (keyIsDown(DOWN_ARROW)) {
+        this.y += this.velocidad;
+      }
+      this.y = constrain(this.y, 0, height - this.alto);
+    }
+  }
+
+  moverAutomatica(pelota) {
+    this.y = pelota.y - this.alto / 2;
+    this.y = constrain(this.y, 0, height - this.alto);
+  }
+}
